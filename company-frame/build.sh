ef46283b995026fbcd1771d3c548815b4cf59f27
hanyunpeng <m13839441583@163.com>
2020-04-03 17:14:06 +0800

    ok commit

--- company-frame/build.sh
+++ company-frame/build.sh
@@ -2,3 +2,5 @@ name=company-frame
 version=latest
 mvn clean compile package -Dmaven.test.skip=true && docker build -t pingxin/${name}:${version} .
 #docker push pingxin/${name}:${version}
+
+#docker run --name company-frame --network px_net --link mysql:data_mysql --link redis:data_redis -p8021:8080  -d pingxin/company-frame:latest
--- spring-boot-shiro-frame/build.sh
+++ spring-boot-shiro-frame/build.sh
@@ -1,4 +1,7 @@
 name=spring-boot-shiro-frame
 version=latest
-mvn clean compile package -Dmaven.test.skip=true && docker build -t ${name} . && docker tag ${name}:${version} pingxin/${name}:${version}
+mvn clean compile package -Dmaven.test.skip=true && docker build -t pingxin/${name}:${version} .
+
 #docker push pingxin/${name}:${version}
+#运行
+#docker run --name spring-boot-shiro-frame --network px_net --link mysql:data_mysql --link redis:data_redis -p8022:8080  -d pingxin/spring-boot-shiro-frame:latest
